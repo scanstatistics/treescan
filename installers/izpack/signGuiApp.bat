6811eb54319c72a42e2915e76e4427a62eba5459
Scott Hostovich <hostovic@imsweb.com>
2015-10-14 15:49:27 +0000

    - updated DFS for Windows signing batch files

--- installers/izpack/signGuiApp.bat
+++ installers/izpack/signGuiApp.bat
@@ -1,6 +1,6 @@
 
 REM Signing a file: (cross-certificate)
-\\nfsf.omni.imsweb.com\treescan\build\treescan\installers\izpack\sign4j\sign4j.exe --verbose \\nfsf.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe sign /v /ac \\nfsf.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\MSCV-VSClass3.cer /f \\nfsf.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\ims-cross-sign.pfx /p %%iaUzoA0cl!p /n "Information Management Services, Inc." /t http://timestamp.verisign.com/scripts/timstamp.dll \\nfsf.omni.imsweb.com\treescan\build\treescan\java_application\jni_application\dist\TreeScan.exe
+\\nfsl.omni.imsweb.com\treescan\build\treescan\installers\izpack\sign4j\sign4j.exe --verbose \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe sign /v /ac \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\MSCV-VSClass3.cer /f \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\ims-cross-sign.pfx /p %%iaUzoA0cl!p /n "Information Management Services, Inc." /t http://timestamp.verisign.com/scripts/timstamp.dll \\nfsl.omni.imsweb.com\treescan\build\treescan\java_application\jni_application\dist\TreeScan.exe
 
 REM To verify the file is signed correctly:
-\\nfsf.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe verify /pa /v \\nfsf.omni.imsweb.com\treescan\build\treescan\java_application\jni_application\dist\TreeScan.exe
+\\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe verify /pa /v \\nfsl.omni.imsweb.com\treescan\build\treescan\java_application\jni_application\dist\TreeScan.exe
