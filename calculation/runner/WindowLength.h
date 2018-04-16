37620289235157731be1ef83847e5ccf358010f2
Scott Hostovich <hostovic@imsweb.com>
2018-04-16 17:34:06 +0000

    * https://www.squishlist.com/ims/treescan/133/  - comment out test statements

--- calculation/runner/WindowLength.h
+++ calculation/runner/WindowLength.h
@@ -56,8 +56,8 @@ class RiskPercentageWindowLength : public AbstractWindowLength {
                     /* r = (ew - q + 1)/(ew - x), => q = -1 * (r(ew - x) - ew - 1) */
                     double q = -1.0 * (_risk_percentage * static_cast<double>(iWindowEnd - _translated_zero_idx) - static_cast<double>(iWindowEnd) - 1.0);
 
-                    int initMinWindowStart = iMinWindowStart;
-                    int initWindowStart = iWindowStart;
+                    //int initMinWindowStart = iMinWindowStart;
+                    //int initWindowStart = iWindowStart;
 
                     //iMinWindowStart = std::max(iMinWindowStart, _translated_zero_idx);
                     iMinWindowStart = std::max(iMinWindowStart, _translated_zero_idx + 1); // minimum is index of true one
@@ -74,12 +74,12 @@ class RiskPercentageWindowLength : public AbstractWindowLength {
                     /* r = (ew - q + 1)/(x - q), => q = (rx - ew - 1)/(r - 1) */
                     double q = (_risk_percentage * (static_cast<double>(_translated_zero_idx)) - static_cast<double>(iWindowEnd) - 1.0) / (_risk_percentage - 1.0);
 
-                    int initWindowStart = iWindowStart;
+                    //int initWindowStart = iWindowStart;
                     iWindowStart = std::min(iWindowStart, static_cast<int>(std::floor(q)));
 
-                    printf("iWindowEnd %d iMinWindowStart,iWindowStart [%d, %d (%.2lf%%)] -> [%d, %d (%.2lf%%)]\n", iWindowEnd,
-                           iMinWindowStart, initWindowStart, 100.0 * static_cast<double>(iWindowEnd - initWindowStart + 1) / static_cast<double>(_translated_zero_idx - initWindowStart + 1),
-                           iMinWindowStart, iWindowStart, 100.0 * static_cast<double>(iWindowEnd - iWindowStart + 1) / static_cast<double>(_translated_zero_idx - iWindowStart + 1));
+                    //printf("iWindowEnd %d iMinWindowStart,iWindowStart [%d, %d (%.2lf%%)] -> [%d, %d (%.2lf%%)]\n", iWindowEnd,
+                    //       iMinWindowStart, initWindowStart, 100.0 * static_cast<double>(iWindowEnd - initWindowStart + 1) / static_cast<double>(_translated_zero_idx - initWindowStart /*+ 1*/),
+                    //       iMinWindowStart, iWindowStart, 100.0 * static_cast<double>(iWindowEnd - iWindowStart + 1) / static_cast<double>(_translated_zero_idx - iWindowStart /*+ 1*/));
                 }
             }
     }
