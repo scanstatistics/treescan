80eb3613d423f15ce83be1adcec84d27d21c28be
Scott Hostovich <hostovichs@imsweb.com>
2021-05-14 15:55:17 -0400

    #149 – csv file source - first row is header

--- calculation/runner/DataSource.cpp
+++ calculation/runner/DataSource.cpp
@@ -99,19 +99,10 @@ bool CSVFileDataSource::readRecord() {
     bool isBlank = true;
     std::string readbuffer;
 
-    if (!_readCount) {
-        // Get the byte-order mark, if there is one
-        unsigned char bom[4];
-        _sourceFile.read(reinterpret_cast<char*>(bom), 4);
-        //Since we don't know what the endian was on the machine that created the file we
-        //are reading, we'll need to check both ways.
-        if ((bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) ||             // utf-8
-            (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) || // UTF-32, big-endian
-            (bom[0] == 0xff && bom[1] == 0xfe && bom[2] == 0 && bom[3] == 0) || // UTF-32, little-endian
-            (bom[0] == 0xfe && bom[1] == 0xff) ||                               // UTF-16, big-endian
-            (bom[0] == 0xff && bom[1] == 0xfe))                                 // UTF-16, little-endian
-            throwUnicodeException();
-        _sourceFile.seekg(0L, std::ios::beg);
+    // skip records as necessary
+    for (long i=_readCount; i < _skip; ++i) {
+        getlinePortable(_sourceFile, readbuffer);
+        ++_readCount;
     }
 
     _read_values.clear();
