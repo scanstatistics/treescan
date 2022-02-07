8a4252516ca8566a8c1dc0701ebf0437af928c96
Scott Hostovich <hostovichs@imsweb.com>
2022-02-07 15:51:17 -0500

    #139 - improved Linux rpm build (work in progress)

--- installers/jpackageInstallerLinux.sh
+++ installers/jpackageInstallerLinux.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
 # Script which creates an installer using Java jpackage.
-# Unfortuntely we can use this at the moment. All the Linux servers at IMS are CentOS
-# and it appears that you need to be on RedHat or Ubuntu. Right now this script works
-# until it attempts to create deb or rpm file. Perhaps I just need other packages installed
-# into my local java or this really needs to be done on RedHat or Ubuntu.
+# Still a work in progress. We can build rpm file on gen-btp-01
+# and that prm is convertable to deb file on Ubuntu. Still:
+# The default template.spec file might not be 100% what we want.
+# I tested this on Ubuntu and it installed but:
+# - permissions are only for root
+# - icon still wrong
+# - probably other things when looking closer
 
-javajdk="/prj/treescan/build/packages/java/jdk-15.0.2-linux_x64"
-version="10.0"
+javajdk="/prj/treescan/build/packages/java/jdk-16.0.2+7-linux_x86"
+version="2.0"
 srcdir="/prj/treescan/build/treescan"
+bundleinputdir="/prj/treescan/build/jpackage/treescanbundlesrc"
 bundledir="/prj/treescan/build/jpackage"
 binaries="/prj/treescan/build/binaries/linux"
 
+rm -rf $bundleinputdir
 rm -rf $bundledir/TreeScan
 rm -rf $bundledir/bin
 
+# copy all input files to bundle input dir
+mkdir -p $bundleinputdir
+cp $srcdir/java_application/jni_application/dist/TreeScan.jar $bundleinputdir
+cp -rf $srcdir/java_application/jni_application/dist/lib $bundleinputdir
+cp -rf $srcdir/installers/examples $bundleinputdir
+cp -f $srcdir/installers/documents/userguide.pdf $bundleinputdir
+cp -f $srcdir/installers/documents/eula.html $bundleinputdir
+cp -f $srcdir/installers/documents/eula/License.txt $bundleinputdir
+cp -f $binaries/treescan32 $bundleinputdir
+cp -f $binaries/libtreescan32.so $bundleinputdir
+cp -f $binaries/treescan64 $bundleinputdir
+cp -f $binaries/libtreescan64.so $bundleinputdir
+
 # Build TreeScan app bundle
-$javajdk/bin/jpackage --verbose --type app-image --input $srcdir/java_application/jni_application/dist --main-jar TreeScan.jar --icon $srcdir/installers/resources/TreeScan.png --app-version $version --name TreeScan --dest $bundledir --java-options "-Djava.library.path=\$APPDIR"
+$javajdk/bin/jpackage --verbose --type app-image --input $bundleinputdir  --main-jar TreeScan.jar --icon $srcdir/installers/resources/TreeScan.png --app-version $version --name TreeScan --dest $bundledir --java-options "-Djava.library.path=\$APPDIR"
 
-# Add additional files to bundle - command-line executables, so, sample data, user guide, etc.
-cp -rf $srcdir/installers/examples $bundledir/TreeScan/examples
-cp -f $srcdir/installers/documents/userguide.pdf $bundledir/TreeScan
-cp -f $srcdir/installers/documents/eula.html $bundledir/TreeScan
-cp -f $srcdir/installers/documents/eula/License.txt $bundledir/TreeScan
-cp -f $binaries/treescan_64 $bundledir/treescan
-cp -f $binaries/libtreescan_64 $bundledir/TreeScan/lib/app/libtreefsscan.so
-
-#  Create application installer.
-$javajdk/bin/jpackage --verbose --type rpm --app-image $bundledir/TreeScan --app-version $version --name TreeScan --dest $bundledir/bin --description "Software for the spatial, temporal, and space-time scan statistics" --vendor "Information Management Services, Inc." --copyright "Copyright 2021, All rights reserved"  --linux-shortcut --linux-menu-group
+#  Create application rpm
+$javajdk/bin/jpackage --verbose --type rpm --app-image $bundledir/TreeScan --app-version $version --name TreeScan  --resource-dir $srcdir/installers/resources --dest $bundledir/bin --description "Software for the spatial, temporal, and space-time scan statistics" --vendor "Information Management Services, Inc." --copyright "Copyright 2021, All rights reserved"  --linux-shortcut --linux-menu-group
 
