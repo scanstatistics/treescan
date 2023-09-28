53674dea11ca0f31a77402d315247c742c92139f
Scott Hostovich <hostovichs@imsweb.com>
2023-09-28 12:10:23 -0400

    #189 - updated gitignore, corrected Mac build scriot

--- scripts/mac/makescript.mac.sh
+++ scripts/mac/makescript.mac.sh
@@ -12,14 +12,14 @@ fi
 echo building zlib library ...
 cd $2/zlib/zlib-1.2.7
 make clean
-make libz.dylib CC="$7 $8 $9" CFLAGS="-stdlib=libc++ -O3 -Wall $4 -DHAVE_HIDDEN  -DHAVE_UNISTD_H" ${11}
+make libz.dylib CC="$7 $8 $9" CFLAGS="-stdlib=libc++ -O3 -Wall $4 -DHAVE_HIDDEN  -DHAVE_UNISTD_H" ${10}
 echo zlib done
 echo
 
 echo building TreeScan binary ...
 cd $2
-make clean TREESCAN=$2 BOOSTDIR=$3 COMPILATION=$4 OPTIMIZATION=$5 INFOPLIST_FILE="$2/scripts/mac/commandline-info.plist" CC="$6 $9 ${10}"
-make TreeScan_mac TREESCAN=$2 BOOSTDIR=$3 COMPILATION=$4 OPTIMIZATION=$5 INFOPLIST_FILE="$2/scripts/mac/commandline-info.plist" CC="$6 $9 ${10}" ${11}
+make clean TREESCAN=$2 BOOSTDIR=$3 COMPILATION=$4 OPTIMIZATION=$5 INFOPLIST_FILE="$2/scripts/mac/commandline-info.plist" CC="$6 $8 $9"
+make TreeScan_mac TREESCAN=$2 BOOSTDIR=$3 COMPILATION=$4 OPTIMIZATION=$5 INFOPLIST_FILE="$2/scripts/mac/commandline-info.plist" CC="$6 $8 $9" ${10}
 strip TreeScan_mac
 mv TreeScan_mac $1
 
